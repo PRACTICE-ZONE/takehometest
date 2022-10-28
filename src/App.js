6cfec6a905ace06299b6eff11883be08735d8a32
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-28 19:01:55 +0300

    Add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Photos field</h1>
+      {/* {message && (
+                <div className="modal-header col-12">
+                  <div className="alert alert-success col-12" role="alert">{message}</div>
+                  <button type="button" onClick={() => { setMessage(''); }} className="btn-close alert-success close-button" data-bs-dismiss="modal" aria-label="close" />
+                </div>
+                )} */}
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-floating mb-2 col-10">
+                    <input
+                      type="file"
+                      name="image"
+                      {...register('image', { required: true })}
+                      className="form-control"
+                      id="floatingInputprice"
+                      placeholder="Upload image"
+                      accept="image/*"
+                    />
+                    <label htmlFor="floatingInputImage">Image</label>
+                  </div>
+                  <div className="form-floating mb-2 col-10">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="caption"
+                      {...register('caption', { required: true })}
+                      id="floatingInput"
+                      placeholder="caption"
+                    />
+                    <label htmlFor="floatingInput">Caption</label>
+                  </div>
+                  <div className="form-floating mb-3 col-10">
+                    <button type="submit" className="btn btn-primary ">Add Car</button>
+                  </div>
+      </form>
     </div>
   );
 }
