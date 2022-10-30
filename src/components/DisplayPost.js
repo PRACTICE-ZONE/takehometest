8f646c86fc1d27de134faf9ddf18ebc27c84079f
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-30 15:56:33 +0300

    Add context api

--- src/components/DisplayPost.js
+++ src/components/DisplayPost.js
@@ -5,15 +5,35 @@ import { AppContext } from '../App'
 
 const DisplayPost = () => {
     const {post, setPost} = useContext(AppContext);
+    
     useEffect(() => {
         axios.get('http://localhost:3000/posts')
         .then((response) => {
-        setPost(response.data.post)
+        setPost(response.data)
+        console.log("Respost", response.data);
         })
     }, [setPost])
+
+    if (post.length === 0) {
+        return (
+          <h1>Loading ...</h1>
+        );
+      }
     
   return (
-    <div className='container border border-info'>{post}</div>
+    <div className='container border border-info'>
+        <div className="row">
+            {
+                post.map((data) => {
+                    return (
+                        <div className="col-3">
+                            {data.caption}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </div>
   )
 }
 
