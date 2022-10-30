a5e5e96c442501f21e447351da725bca4cf50e1b
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-30 07:30:07 +0300

    Update the readme file

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
-
-import { useForm } from 'react-hook-form';
+import AddPost from "./components/AddPost";
+import DisplayPost from "./components/DisplayPost";
 
 function App() {
-  const { register, handleSubmit, errors } = useForm();
-  const [message, setMessage] = useState('')
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
+  
   return (
     <div className="container">
-      <h1>Photos field</h1>
-      {/* {message && (
-                <div className="modal-header col-12">
-                  <div className="alert alert-success col-12" role="alert">{message}</div>
-                  <button type="button" onClick={() => { setMessage(''); }} className="btn-close alert-success close-button" data-bs-dismiss="modal" aria-label="close" />
-                </div>
-                )} */}
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-floating mb-2 col-10">
-                    <input
-                      type="file"
-                      name="image"
-                      {...register('image', { required: true })}
-                      className="form-control"
-                      id="floatingInputprice"
-                      placeholder="Upload image"
-                      accept="image/*"
-                    />
-                    <label htmlFor="floatingInputImage">Image</label>
-                  </div>
-                  <div className="form-floating mb-2 col-10">
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="caption"
-                      {...register('caption', { required: true })}
-                      id="floatingInput"
-                      placeholder="caption"
-                    />
-                    <label htmlFor="floatingInput">Caption</label>
-                  </div>
-                  <div className="form-floating mb-3 col-10">
-                    <button type="submit" className="btn btn-primary ">Add Car</button>
-                  </div>
-      </form>
+     <AddPost />
+     <DisplayPost />
     </div>
   );
 }
---
+++ src/components/DisplayPost.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DisplayPost = () => {
+  return (
+    <div className='container border border-info'>DisplayPost</div>
+  )
+}
+
+export default DisplayPost
